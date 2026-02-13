932a76546ab51028cd61ecb817c334a72cf03cbf
yelgbrl <santosogabriel001@gmail.com>
2026-02-13 16:37:30 +0700

    Perbaikan kode p042OperatorAritmatika.java

--- Pertemuan 4/p042OperatorAritmatika.java
+++ Pertemuan 4/p042OperatorAritmatika.java
@@ -4,19 +4,19 @@ Nama  : Gabriel Santoso
 Program untuk p042 OperatorAritmatika Pertemuan 4
  */
 public class p042OperatorAritmatika {
-public static void main(String[] args) {
-int a = 10;
-int b = 3;
-System.out.println("a = " + a);
-System.out.println("b = " + b);
-System.out.println();
-System.out.println("a + b = " + (a + b));
-System.out.println("a - b = " + (a - b));
-System.out.println("a * b = " + (a * b));
-System.out.println("a / b = " + (a / b));
-System.out.println("a % b = " + (a % b));
-// Pembagian dengan desimal
-double hasil = (double) a / b;
-System.out.println("a / b (desimal) = " + hasil);
-}
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 3;
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println();
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+    // Pembagian dengan desimal
+    double hasil = (double) a / b;
+    System.out.println("a / b (desimal) = " + hasil);
+  }
 }
