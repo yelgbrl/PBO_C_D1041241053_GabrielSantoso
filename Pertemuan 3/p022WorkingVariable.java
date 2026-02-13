3b84355f4d2c5ec42b46798851d364f89b60391c
yelgbrl <santosogabriel001@gmail.com>
2026-02-13 14:33:09 +0700

    Initial commit

---
+++ Pertemuan 3/p022WorkingVariable.java
@@ -0,0 +1,10 @@
+public class p022WorkingVariable {
+  public static void main(String[] args) {
+    int angka = 10;
+
+    System.out.println("Nilai awal angka: " + angka);
+
+    angka = 25;
+    System.out.println("Nilai angka setelah diubah: " + angka);
+  }
+}
---
+++ Pertemuan 3/p023PrimitiveData.java
@@ -0,0 +1,27 @@
+
+
+public class p023PrimitiveData {
+  public static void main(String[] args) {
+    
+    // Tipe data bilangan bulat
+    int umur = 20;
+    long populasi = 000000000L;
+
+    // Tipe data bilangan desimal
+    float tinggi = 170.5f;
+    double ipk = 3.75;
+    // Tipe data karakter
+    char nilaiHuruf = 'A';
+
+    //Tipe data logika
+    boolean lulus = true;
+
+    // Menampilkan nilai semua variabel
+    System.out.println("Umur          : " + umur);
+    System.out.println("Populasi      : " + populasi);
+    System.out.println("Tinggi Badan  : " + tinggi);
+    System.out.println("IPK           : " + ipk);
+    System.out.println("Nilai Huruf   : " + nilaiHuruf);
+    System.out.println("Status Lulus  : " + lulus);
+  }  
+}
