0bfcd5224f97b2a8bb7222284e82683c0e99bd23
yelgbrl <santosogabriel001@gmail.com>
2026-02-14 16:23:09 +0700

    Merevisi file LatihanKalkulator : Menambahkan Pembagian Desimal

--- Pertemuan 4/LatihanKalkulator.java
+++ Pertemuan 4/LatihanKalkulator.java
@@ -6,7 +6,7 @@ Latihan operasi aritmatika menggunakan bahasa Java
 public class LatihanKalkulator {
   public static void main(String[] args) {
     int angka1 = 25, angka2 = 4;
-
+    double pembagianDesimal = (double) angka1 / angka2;
     System.out.println("=== KALKULATOR ===");
     System.out.println("Angka 1: " + angka1);
     System.out.println("Angka 2: " + angka2);
@@ -16,5 +16,6 @@ public class LatihanKalkulator {
     System.out.println("Perkalian: " + (angka1 * angka2));
     System.out.println("Pembagian: " + (angka1 / angka2));
     System.out.println("Sisa Bagi: " + (angka1 % angka2));
+    System.out.println("Pembagian Desimal: " + pembagianDesimal);
   }
 }
