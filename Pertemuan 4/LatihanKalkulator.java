9e0402c888dc778e8e072723a58a1aacedc25b30
yelgbrl <santosogabriel001@gmail.com>
2026-02-14 16:15:27 +0700

    Menambahkan LatihanKalkulator (Latihan 2) ke Pertemuan 4

---
+++ Pertemuan 4/LatihanKalkulator.java
@@ -0,0 +1,20 @@
+/*
+NIM   : D1041241053
+Nama  : Gabriel Santoso
+Latihan operasi aritmatika menggunakan bahasa Java
+ */
+public class LatihanKalkulator {
+  public static void main(String[] args) {
+    int angka1 = 25, angka2 = 4;
+
+    System.out.println("=== KALKULATOR ===");
+    System.out.println("Angka 1: " + angka1);
+    System.out.println("Angka 2: " + angka2);
+    System.out.println("---");
+    System.out.println("Penjumlahan: " + (angka1 + angka2));
+    System.out.println("Pengurangan: " + (angka1 - angka2));
+    System.out.println("Perkalian: " + (angka1 * angka2));
+    System.out.println("Pembagian: " + (angka1 / angka2));
+    System.out.println("Sisa Bagi: " + (angka1 % angka2));
+  }
+}
