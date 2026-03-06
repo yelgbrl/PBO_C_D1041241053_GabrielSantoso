9e9e6f0de3d011ccf1877eff1b4d58eb1c35ba84
yelgbrl <santosogabriel001@gmail.com>
2026-03-06 12:54:02 +0700

    Adding LatihanClassBuku

---
+++ Pertemuan 9/LatihanClassBuku.java
@@ -0,0 +1,33 @@
+class Buku {
+  String judul;
+  String pengarang;
+  int harga;
+
+  public void tampilkanInfo() {
+    System.out.println("=== INFORMASI BUKU ===");
+    System.out.println();
+    System.out.println("Judul     : " + judul);
+    System.out.println();
+    System.out.println("Pengarang : " + pengarang);
+    System.out.println();
+    System.out.println("Harga     : Rp " + harga);
+    System.out.println();
+  }
+}
+
+public class LatihanClassBuku {
+  public static void main(String[] args) {
+    Buku buku1 = new Buku();
+    buku1.judul = "Pemrograman Java";
+    buku1.pengarang = "Budi Raharjo";
+    buku1.harga = 125000;
+
+    Buku buku2 = new Buku();
+    buku2.judul = "Algoritma dan Struktur Data";
+    buku2.pengarang = "Rinaldi Munir";
+    buku2.harga = 150000;
+
+    buku1.tampilkanInfo();
+    buku2.tampilkanInfo();
+  }
+}
